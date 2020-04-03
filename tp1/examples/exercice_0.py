83004a234f29570dc4458dcc32327494211ce93c
sacha <sachdu84@hotmail.fr>
2020-04-03 14:46:17 +0200

    ddd

---
+++ tp1/examples/exercice_0.py
@@ -0,0 +1,11 @@
+a = 5.3
+b = 9.70
+c = a + b
+print(c)
+c += a
+d = c
+d -= 1
+
+print("la valeur de c à la fin du programme est :",c)
+print("la valeur de d à la fin du programme est :",d)
+print(type(c))
